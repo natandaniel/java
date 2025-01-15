a2bb0c5f6d13ea83bba21ff560edbab4e787b3f3
natandaniel <natan.daniel0@gmail.com>
2025-01-15 16:53:38 +0100

    adding test support and JdbcTemplateTest class

---
+++ springframework/jdbc/src/test/java/coffees/JdbcTemplateTest.java
@@ -0,0 +1,21 @@
+package coffees;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { CoffeesAppConfig.class })
+class JdbcTemplateTest {
+  @Autowired
+  private JdbcTemplate mysqlJdbcTemplate;
+
+  @Test
+  void toto() {
+
+  }
+
+}
