6ae54795ea7247842c0271181bcbc44d297d66ed
natandaniel <natan.daniel0@gmail.com>
2025-01-17 14:58:47 +0100

    adding JdbcTemplate batch update test

--- springframework/jdbc/src/test/java/jdbc/JdbcTemplateUpdatingTests.java
+++ springframework/jdbc/src/test/java/jdbc/JdbcTemplateUpdatingTests.java
@@ -6,9 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -50,7 +56,7 @@ public class JdbcTemplateUpdatingTests {
 
     assertNotNull(coffee);
     assertEquals("Ethiopian", coffee.getName());
-    assertEquals(10.99, coffee.getPrice(), 0.01);
+    assertEquals(10.99f, coffee.getPrice());
   }
 
   // updating an existing entry
@@ -69,7 +75,7 @@ public class JdbcTemplateUpdatingTests {
 
     assertNotNull(coffee);
     assertEquals("Colombian", coffee.getName());
-    assertEquals(8.99, coffee.getPrice(), 0.01);
+    assertEquals(8.99f, coffee.getPrice());
   }
 
   // deleting an existing entry
@@ -81,6 +87,49 @@ public class JdbcTemplateUpdatingTests {
 
     assertEquals(13, mysqlJdbcTemplate.queryForObject("select count(*) from COFFEE_HOUSES",
         Integer.class));
+  }
+
+  // batch insertions
+
+  @Test
+  void testInsertCoffeeRecordBatch() {
+    List<Coffee> coffeeBatch = new ArrayList<>();
+    coffeeBatch.add(new Coffee("Coffee1", 1.99f));
+    coffeeBatch.add(new Coffee("Coffee2", 1.99f));
+    coffeeBatch.add(new Coffee("Coffee3", 1.99f));
+    coffeeBatch.add(new Coffee("Coffee4", 1.99f));
+
+    mysqlJdbcTemplate.batchUpdate(
+        "insert into COFFEES (cof_name, sup_id, price, sales, total) values (?, ?, ?, ?, ?)",
+        new BatchPreparedStatementSetter() {
+          @Override
+          public void setValues(PreparedStatement ps, int i) throws SQLException {
+            Coffee coffee = coffeeBatch.get(i);
+            ps.setString(1, coffee.getName());
+            ps.setInt(2, 49);
+            ps.setFloat(3, coffee.getPrice());
+            ps.setInt(4, 0);
+            ps.setInt(5, 0);
+          }
+
+          @Override
+          public int getBatchSize() {
+            return coffeeBatch.size();
+          }
+        });
+
+    assertEquals(9, mysqlJdbcTemplate.queryForObject("select count(*) from COFFEES",
+        Integer.class));
+
+    Coffee coffee =
+        mysqlJdbcTemplate.queryForObject("select cof_name, price from COFFEES where cof_name = ?",
+            (resultSet, rowNum) ->
+                new Coffee(resultSet.getString("cof_name"), resultSet.getFloat("price")),
+            "Coffee3");
+
+    assertNotNull(coffee);
+    assertEquals("Coffee3", coffee.getName());
+    assertEquals(1.99f, coffee.getPrice());
   }
 
 }
