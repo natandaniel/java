5d1be4ab116a8d7d7fc46088064c2d975857d212
natandaniel <natan.daniel0@gmail.com>
2025-01-25 13:06:51 +0100

    adding tests

--- spring-data-jdbc/src/test/java/jdbc/CoffeeHouseRepositoryTests.java
+++ spring-data-jdbc/src/test/java/jdbc/CoffeeHouseRepositoryTests.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = { JdbcConfiguration.class })
 @Sql({ "/create-tables.sql", "/populate-tables.sql" })
@@ -18,7 +20,56 @@ public class CoffeeHouseRepositoryTests {
   @Test
   void testFindingCoffeeHouseByName() {
     CoffeeHouse coffeeHouse = coffeeHouseRepository.findByName("Relaxing Coffee");
-    System.out.println();
+    assertNotNull(coffeeHouse);
+    assertEquals("Relaxing Coffee", coffeeHouse.getName());
+  }
+
+  @Test
+  void testAddingANewCoffeeBeansProductToCoffeeHouseNamedRelaxingCoffee() {
+    CoffeeHouse coffeeHouse = coffeeHouseRepository.findByName("Relaxing Coffee");
+
+    coffeeHouse.addCoffeeBeans(new Product("Fabulous Roast", 45.99f, "a premium quality dark " +
+        "roast", 500.00f), CoffeeBeanType.ARABICA, 100);
+
+    coffeeHouseRepository.save(coffeeHouse);
+
+    assertTrue(coffeeHouseRepository.findByName("Relaxing Coffee").getCoffeeBeans("Fabulous " +
+        "Roast").isPresent());
+  }
+
+  @Test
+  void testRestockingACoffeeBeansProductForCoffeeHouseNamedRelaxingCoffee() {
+    CoffeeHouse coffeeHouse = coffeeHouseRepository.findByName("Relaxing Coffee");
+    coffeeHouse.restockCoffeeBeans("Mountain Roast", 50);
+
+    coffeeHouseRepository.save(coffeeHouse);
+
+    CoffeeHouse relaxingCoffee = coffeeHouseRepository.findByName("Relaxing Coffee");
+    CoffeeBeans coffeeBeans = relaxingCoffee.getCoffeeBeans("Mountain Roast")
+                                            .orElseThrow();
+
+    assertEquals(100, coffeeBeans.stockLevel());
+  }
+
+  @Test
+  void testSellingACoffeeBeansProductFromCoffeeHouseNamedRelaxingCoffee() {
+    CoffeeHouse coffeeHouse = coffeeHouseRepository.findByName("Relaxing Coffee");
+    coffeeHouse.sellCoffeeBeans("Mountain Roast", 20);
+
+    coffeeHouseRepository.save(coffeeHouse);
+
+    CoffeeHouse relaxingCoffee = coffeeHouseRepository.findByName("Relaxing Coffee");
+    CoffeeBeans coffeeBeans = relaxingCoffee.getCoffeeBeans("Mountain Roast")
+                                            .orElseThrow();
+
+    // checking consistent inventory
+    assertEquals(30, coffeeBeans.stockLevel());
+
+    // checking consistent sales record
+    Sale sale = relaxingCoffee.getLatestSale().orElseThrow();
+    assertEquals("Mountain Roast", sale.productName());
+    assertEquals(20, sale.quantity());
+    assertEquals(219.8f, sale.totalPrice());
   }
 
 }
