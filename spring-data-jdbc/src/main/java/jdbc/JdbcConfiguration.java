35ed2613f940232f046957fb447a225f1cf1bf43
natandaniel <natan.daniel0@gmail.com>
2025-01-25 13:44:24 +0100

    adding sample event and callback code

--- spring-data-jdbc/src/main/java/jdbc/JdbcConfiguration.java
+++ spring-data-jdbc/src/main/java/jdbc/JdbcConfiguration.java
@@ -1,12 +1,17 @@
 package jdbc;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.data.relational.core.mapping.event.BeforeSaveCallback;
+import org.springframework.data.relational.core.mapping.event.RelationalEvent;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -18,7 +23,7 @@ import java.util.List;
 @Configuration
 @EnableJdbcRepositories("jdbc")
 @PropertySource("classpath:database.properties")
-public class JdbcConfiguration extends AbstractJdbcConfiguration {
+class JdbcConfiguration extends AbstractJdbcConfiguration {
   @Value("${mysql.host}")
   private String mysqlDatabaseHost;
   @Value("${mysql.dbname}")
@@ -51,6 +56,23 @@ public class JdbcConfiguration extends AbstractJdbcConfiguration {
   @Override
   protected List<?> userConverters() {
     return List.of(new StringToCoffeeMakerTypeConverter());
+  }
+
+  @Bean
+  public ApplicationListener<?> loggingListener() {
+    return (ApplicationListener<ApplicationEvent>) event -> {
+      if (event instanceof RelationalEvent) {
+        LoggerFactory.getLogger(JdbcConfiguration.class).info("Received an event: {}", event);
+      }
+    };
+  }
+
+  @Bean
+  public BeforeSaveCallback<CoffeeHouse> timeStampingSaveTime() {
+    return (entity, aggregateChange) -> {
+      LoggerFactory.getLogger(JdbcConfiguration.class).info("saving coffee house");
+      return entity;
+    };
   }
 
 }
