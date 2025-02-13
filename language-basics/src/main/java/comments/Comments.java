5c79f8354983974e1e109e0eb2549c84b87f147e
natandaniel <natan.daniel0@gmail.com>
2025-02-13 20:33:45 +0100

    formatting

--- language-basics/src/main/java/comments/Comments.java
+++ language-basics/src/main/java/comments/Comments.java
@@ -1,14 +1,29 @@
 package comments;
 
 /**
- * <p>This is a specialised form of multi-line comments called Javadoc.</p> <p>Javadoc starts
- * with a forward slash / followed by two asterisks ** and ends with an asterisk * followed by a
- * forward slash /.</p> <p>Javadoc is used to document classes, interfaces, enums, annotations,
- * records and methods.</p> <p>The javadoc program parses Java source code to generate a
- * documentation based on the detected Javadoc.</p> <p>Javadoc can use elements of the HTML syntax
- * for structuring its text (paragraphs, line breaks, emphasis, ...)</p><p> Finally, Javadoc can
- * also use instructions starting with the at sign @ (@author, @param, @return, @version, ...) that
- * the javadoc program processes differently resulting in a .</p>
+ * <p>
+ * This is a specialised form of multi-line comments called Javadoc.
+ * </p>
+ * <p>
+ * Javadoc starts with a forward slash / followed by two asterisks ** and ends with an asterisk *
+ * followed by a forward slash /.
+ * </p>
+ * <p>
+ * Javadoc is used to document classes, interfaces, enums, annotations, records and methods.
+ * </p>
+ * <p>
+ * The javadoc program parses Java source code to generate a documentation based on the detected
+ * Javadoc.
+ * </p>
+ * <p>
+ * Javadoc can use elements of the HTML syntax for structuring its text (paragraphs, line breaks,
+ * emphasis, ...)
+ * </p>
+ * <p>
+ * Finally, Javadoc can also use instructions starting with the at sign @ (@author, @param ,@return
+ * , @version, ...) that the javadoc program processes differently resulting in a special
+ * presentation.
+ * </p>
  *
  * @version 1.0
  */
